f707cb28e2e55b97347a31eaa78330b46ccbffb7
al-isa <nawwafaaleisa@gmail.com>
2025-09-30 13:42:59 -0400

    working search engine

--- src/index.cpp
+++ src/index.cpp
@@ -1,14 +1,22 @@
 #include "index.h"
 #include "tokenizer.h"
 #include <algorithm> //for std::find
+#include <unordered_set>
+#include <iostream>
 
 //add one documents token into the index
 void InvertedIndex::add_document(const Document& doc) {
     auto tokens = tokenize(doc.content); //break text into words
+    //track if we already added this doc for a given word
+    std::unordered_set<std::string> seen_tokens;
     for (const auto& token : tokens) {
         //avoid duplicate doc IDs if word appears multiple times
-        if (index[token].empty() || index[token].back() != doc.id) {
+        if (seen_tokens.find(token) == seen_tokens.end()) {
             index[token].push_back(doc.id);
+            seen_tokens.insert(token);
+
+            //std::cout << "Adding '" << token << "' to index for doc " << doc.id << "\n";
+
         }
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,45 @@ Document load_document(const std::string& filepath, int id);
 int main() {
     //load 3 text file into memory
     Document doc1 = load_document("../data/doc1.txt", 1);
-    Document doc2 = load_document("../data/doc2.txt", 1);
-    Document doc3 = load_document("../data/doc3.txt", 1);
+    Document doc2 = load_document("../data/doc2.txt", 2);
+    Document doc3 = load_document("../data/doc3.txt", 3);
+
 
     //print the contents of each document just to check if it worked
     // std::cout << "Doc1 contents: " << doc1.content << "\n";
     // std::cout << "Doc2 contents: " << doc2.content << "\n";
     // std::cout << "Doc3 contents: " << doc3.content << "\n";
 
-    // //tokenize its contents
-    // std::vector<std::string> tokens = tokenize(doc1.content);
+    //tokenize its contents
+    std::vector<std::string> tokens = tokenize(doc1.content);
 
-    // //print the tokens
-    // std::cout << "Tokens from Doc1:" << std::endl;
-    // for (const auto& t : tokens) { 
-    //     std::cout << t << " "; //create a space between each token
-    // }
-    // std::cout << std::endl;
+    //print the tokens
+    std::cout << "Tokens from Doc1:" << std::endl;
+    for (const auto& t : tokens) { 
+        std::cout << t << " "; //create a space between each token
+    }
+
+    std::cout << std::endl;
+
+    //tokenize its contents
+    tokens = tokenize(doc2.content);
+
+    //print the tokens
+    std::cout << "Tokens from Doc2:" << std::endl;
+    for (const auto& t : tokens) { 
+        std::cout << t << " "; //create a space between each token
+    }
+    std::cout << std::endl;
+
+    //tokenize its contents
+    tokens = tokenize(doc3.content);
+
+    //print the tokens
+    std::cout << "Tokens from Doc3:" << std::endl;
+    for (const auto& t : tokens) { 
+        std::cout << t << " "; //create a space between each token
+    }
+    std::cout << std::endl;
 
     //create inverted index
     InvertedIndex idx;
@@ -37,7 +59,7 @@ int main() {
     std::string query = "cat";
     auto results = idx.search(query);
 
-    std::cout << "Search Results for ' " << query << "': ";
+    std::cout << "Search Results for '" << query << "': ";
     for (int id : results) {
         std::cout << id << " ";
     }
